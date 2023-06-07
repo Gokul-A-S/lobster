ed484b503c80be49250cdea6bd06b008305e8c18
Gokul-A-S <gokuld271@gmail.com>
2023-06-07 17:21:16 +0530

    Fix the bug in the Alerts page

--- client/src/pages/Alerts.js
+++ client/src/pages/Alerts.js
@@ -33,42 +33,4 @@ const Alerts=()=>{
         </div>
     )
 }
-export default Alerts
-
-import React,{useEffect,useState} from 'react'
-import AlertDetails from '../components/AlertDetails'
-const Alerts=()=>{
-    const [alerts,setAlerts]=useState([])
-    useEffect(()=>{
-        const getAlerts=async()=>{
-            try{
-                const response=await fetch(`${process.env.REACT_APP_SERVER_URI}/api/alerts`)
-                const json=await response.json()
-                if (response.ok){
-                    setAlerts(json)
-                    console.log(json)
-                }
-                if(!response.ok){
-                    console.log(json)
-                }
-            }
-            catch(error){
-                console.log(error.message)
-            }
-        }
-        getAlerts()
-    },[])
-    return(
-        <div>
-            <h1>Alerts</h1>
-            {alerts && alerts.map((alerts,index)=>(
-                <div key={index}>
-                    <AlertDetails alerts={alerts} key={alerts._id} />
-                </div>
-            )
-            )}
-        </div>
-    )
-}
-export default Alerts
 
