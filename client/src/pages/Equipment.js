dcfd07e63e08ee58af845b8c1bac5f1f6c423ab8
Hrishi-Krish <hrishasur@gmail.com>
2023-06-11 08:57:58 +0530

    changes in CSS, changed lobster redirection to home page

--- client/src/pages/Equipment.js
+++ client/src/pages/Equipment.js
@@ -1,7 +1,10 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import moment from 'moment';
 const Equipment = () => {
     const eqp = useLocation().state.workout
     //console.log(eqp)
+    const purchaseDate = moment(eqp.dop).format('DD-MM-YYYY')
+    const warrantyDate = moment(eqp.warranty).format('DD-MM-YYYY')
     return (
         <div className="equipment-view">
             <div className="image">
@@ -10,10 +13,10 @@ const Equipment = () => {
                 <h2>Name: <span className="value">{eqp.name}</span></h2>
                 <h2>Type: <span className="value">{eqp.type}</span></h2>
                 <h2>Brand: <span className="value">{eqp.brand}</span></h2>
-                <h2>Date of Purchase: <span className="value">{eqp.dop}</span></h2>
-                <h2>Warranty: <span className="value">{eqp.warranty}</span></h2>
+                <h2>Purchase Date: <span className="value">{purchaseDate}</span></h2>
+                <h2>Warranty: <span className="value">{warrantyDate}</span></h2>
                 <h2>Condition: <span className="value">{eqp.condition}</span></h2>
-                <h2>Current Location: <span className="value">{eqp.location}</span></h2>
+                <h2>Location: <span className="value">{eqp.location}</span></h2>
                 <h2>Lab: <span className="value">{eqp.lab}</span></h2>
 
             </div>
