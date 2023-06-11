7bc979e54cf61d501e837c38763ab7d3485f6a45
Hrishi-Krish <hrishasur@gmail.com>
2023-06-11 12:10:22 +0530

    Added icon to equipment page (idk if it's needed) and renamed view in lab to details

--- client/src/pages/LabView.js
+++ client/src/pages/LabView.js
@@ -3,17 +3,11 @@ const LabView = () => {
     const lab=useLocation().state.labs
     return (
         <div className="lab">
-            <h1 className="heading">Lab Details</h1> <br />
-            <div className="lab-details-container">
-            <div className="lab-details">
-                <h2>Name:</h2><span className="value">{lab.name}</span>
-            </div>
-            <div className="lab-details">
-                <h2>Code: </h2><span className="value">{lab.code}</span>
-            </div>
-            <div className="lab-details">
-                <h2>Faculty: </h2><span className="value">{lab.fic}</span>    
-            </div>
+            <h1 className="heading">Lab Details</h1>
+            <div className="info">
+                <h2>Name <span className="value">{lab.name}</span> </h2>
+                <h2>Code <span className="value">{lab.code}</span> </h2>
+                <h2>Faculty <span className="value">{lab.fic}</span> </h2>    
             </div>
             
             
