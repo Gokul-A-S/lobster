b8ed98e2801c48d1317342600eb2939bbf605b8a
Gokul-A-S <gokuld271@gmail.com>
2023-06-14 22:53:51 +0530

    Alert page icon

--- client/src/components/AlertDetails.js
+++ client/src/components/AlertDetails.js
@@ -1,8 +1,9 @@
+import logo from '../logo.png'
 const AlertDetails = ({alerts}) => {
     return (
-        <div className="equipment-view">
-                    <div className="image">
-                        <img src={''} alt="equipment" /></div>
+        <div className="computer-logo">
+                    <div className="lab-computer">
+                        <img src={logo} alt="equipment" /></div>
                     <div className="info">
                         <h2>Name: <span className="value">{alerts.name}</span></h2>
                         <h2>Type: <span className="value">{alerts.type}</span></h2>
@@ -10,7 +11,6 @@ const AlertDetails = ({alerts}) => {
                         <h2>Date of Purchase: <span className="value">{alerts.dop}</span></h2>
                         <h2>Warranty: <span className="value">{alerts.warranty}</span></h2>
                         <h2>Condition: <span className="value">{alerts.condition}</span></h2>
-                        <h2>Location: <span className="value">{alerts.location}</span></h2>
                         <h2>Lab: <span className="value">{alerts.lab}</span></h2>
         
                     </div>
