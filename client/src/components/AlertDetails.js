9a7cc8bb40029be34960c84f3da1c568a548808c
Gokul-A-S <gokuld271@gmail.com>
2023-06-25 21:17:48 +0530

    Hotfix

--- client/src/components/AlertDetails.js
+++ client/src/components/AlertDetails.js
@@ -4,11 +4,13 @@ const AlertDetails = ({alerts}) => {
     return (
         <div className="computer-logo">
                     <div className="alert-info">
-                        <h2>Name: <span className="value">{alerts.name}</span></h2>
-                        <h2>Type: <span className="value">{alerts.type}</span></h2>
-                        <h2>Date of Purchase: <span className="value">{format(new Date(alerts.dop), 'dd-MM-yyyy')}</span></h2>
-                        <h2>Warranty: <span className="value">{format(new Date(alerts.warranty),'dd-MM-yyyy')}</span></h2>
-                        <h2>Lab: <span className="value">{alerts.lab}</span></h2>
+                    <div className="computer-logo">
+                    <div className="alert-info">
+                        <h2><span className="value">{alerts.name}</span></h2>
+                        <p>Warranty Expiring for {alerts.name} on {format(new Date(alerts.warranty),'dd-MM-yyyy')}</p>
+                        <p>Lab: {alerts.lab}</p>
+                    </div>
+                </div>
         
                     </div>
                 </div>
--- client/src/pages/Equipment.js
+++ client/src/pages/Equipment.js
@@ -3,18 +3,20 @@ import { format } from 'date-fns'
 const Equipment = () => {
     const eqp = useLocation().state.workout
     const purchaseDate = format(new Date(eqp.dop), 'dd-MM-yyyy')
-    const warrantyDate = format(new Date(eqp.dop), 'dd-MM-yyyy')
+    const warrantyDate = format(new Date(eqp.warranty), 'dd-MM-yyyy')
     return (
         <div className="equipment-view">
+                <h1>{eqp.name}</h1>
             <div className="info">
-                <h2>Name <span className="value">{eqp.name}</span></h2>
                 <h2>Type <span className="value">{eqp.type}</span></h2>
                 <h2>Brand <span className="value">{eqp.brand}</span></h2>
                 <h2>Purchase Date <span className="value">{purchaseDate}</span></h2>
                 <h2>Warranty <span className="value">{warrantyDate}</span></h2>
                 <h2>Condition <span className="value">{eqp.condition}</span></h2>
                 <h2>Lab <span className="value">{eqp.lab}</span></h2>
-
+                <h2>Processor <span className="value">{eqp.processor}</span></h2>
+                <h2>Ram <span className="value">{eqp.ram}GB</span></h2>
+                <h2>HDD <span className="value">{eqp.hdd}GB</span></h2>
             </div>
         </div>
 
