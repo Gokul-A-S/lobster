97a5455dfcd31be0986cb624be0dd5d117bf078c
Gokul-A-S <gokuld271@gmail.com>
2023-06-25 13:11:56 +0530

    Blue

--- client/src/components/AlertDetails.js
+++ client/src/components/AlertDetails.js
@@ -6,10 +6,8 @@ const AlertDetails = ({alerts}) => {
                     <div className="alert-info">
                         <h2>Name: <span className="value">{alerts.name}</span></h2>
                         <h2>Type: <span className="value">{alerts.type}</span></h2>
-                        <h2>Brand: <span className="value">{alerts.brand}</span></h2>
                         <h2>Date of Purchase: <span className="value">{format(new Date(alerts.dop), 'dd-MM-yyyy')}</span></h2>
                         <h2>Warranty: <span className="value">{format(new Date(alerts.warranty),'dd-MM-yyyy')}</span></h2>
-                        <h2>Condition: <span className="value">{alerts.condition}</span></h2>
                         <h2>Lab: <span className="value">{alerts.lab}</span></h2>
         
                     </div>
